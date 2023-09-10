891ce12faadfe45263f9fcc7c3e6924564e64288
Zormelo_Alex <alexzormelo9@gmail.com>
2023-09-10 23:00:01 +0000

    more posts

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,40 @@ import Navbar from '../components/Navbar'
 import {AiOutlineSearch} from "react-icons/ai"
 
 const Home = () => {
+
+    const posts = [
+      {
+        imgUrl: "https://wallpaperaccess.com/full/9070071.png",
+        title: "Navigating the Digital Age: Empowering Your Online Presence",
+        createDate: "October 4, 2023",
+        username: "Zormelo Alex Dodzi",
+      },
+      {
+        imgUrl: "https://wallpaperaccess.com/full/9070119.png",
+        title: "The Science of Happiness: Discovering Joy in Everyday Life",
+        createDate: "October 4, 2023",
+        username: "Ashafa Ahmed",
+      },
+      {
+        imgUrl: "https://wallpaperaccess.com/full/8504301.png",
+        title: "Sustainable Living: Redefining Your Eco-Friendly Lifestyle",
+        createDate: "October 4, 2023",
+        username: "Morrison Jessica",
+      },
+      {
+        imgUrl: "https://wallpaperaccess.com/full/9070106.jpg",
+        title: "Beyond Boundaries: Exploring the Beauty of World Travel",
+        createDate: "October 4, 2023",
+        username: "Normanyo-Grives Etornam",
+      },
+      {
+        imgUrl: "https://wallpaperaccess.com/full/9070100.jpg",
+        title: "From Dreams to Reality: Achieving Your Goals with Determination",
+        createDate: "October 4, 2023",
+        username: "Laurene Adjei",
+      },
+    ];
+    
   return (
     <>
         <Navbar/>
@@ -23,11 +57,34 @@ const Home = () => {
                     <div className="userinfo flex gap-3 mt-2 text-gray-500">
                         <div className="date">October 4, 2023</div>
                         <div className="date">-</div>
-                        <div className="username">Zormelo Alex</div>
+                        <div className="username">Zormelo Alex Dodzi</div>
                     </div>
                 </div>
             </div>
             <hr className='mt-8' />
+            <div className="morePosts mt-8">
+                <div className="top flex justify-between items-center">
+                    <h2 className='text-2xl font-semibold'>More Posts</h2>
+                    <p className='text-gray-500 capitalize'>see all</p>
+                </div>
+                <div className="posts mt-4">
+                    {posts.map((post, index)=>(
+                        <div className="post flex gap-5 items-center mb-4">
+                            <div className="image">
+                                <img src={post.imgUrl} alt="post image" />
+                            </div>
+                            <div className="postdetails">
+                                <div className="title font-semibold text-lg">{post.title}</div>
+                                <div className="userinfo flex gap-3 text-sm text-gray-500">
+                                    <div className="date">{post.createDate}</div>
+                                    <div className="date">-</div>
+                                    <div className="username">{post.username}</div>
+                                </div>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+            </div>
         </div>
     </>
   )
