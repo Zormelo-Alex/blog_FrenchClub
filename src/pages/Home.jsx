7f6ccdef67735da2a442e75be038b6fde799417c
Zormelo_Alex <alexzormelo9@gmail.com>
2023-09-10 21:38:56 +0000

    search

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
+import {AiOutlineSearch} from "react-icons/ai"
 
 const Home = () => {
   return (
     <>
         <Navbar/>
-        <div className='home pt-4'>
+        <div className='home pt-8 pl-4 pr-4'>
             <div className="search">
                 <input type="search" placeholder='Search for a topic...' />
+                <AiOutlineSearch/>
             </div>
         </div>
     </>
