55802107cbe63974cc50e9d1595718dbbc28f4eb
Zormelo_Alex <alexzormelo9@gmail.com>
2023-09-14 14:19:44 +0000

    added loader

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Loading = () => {
   return (
-    <div>
+    <div className='w-full flex items-center justify-center mt-8'>
         <div className="animate-spin rounded-full h-8 w-8 border-t-2 border-b-2 border-blue-500"></div>
     </div>
   )
